90ceb39a45cf38d91d833b311c892112b99a94c9
kaizostock014-dotcom <kaizostock014@gmail.com>
2026-09-19 10:08:08 -0500

    Update greeting message in Index.php

--- Index.php
+++ Index.php
@@ -23,7 +23,7 @@ $message_id = $update["message"]["message_id"];
 //////////=========[Start Command]=========//////////
 
 if ((strpos($message, "!start") === 0)||(strpos($message, "/start") === 0)){
-sendMessage($chatId, "<b>¡Hola! 👋\nEscribe /help para ver todos mis comandos.\n\nBot creado por LapanWasTaken @Cendawannn</b>");
+sendMessage($chatId, "<b>¡Hola! 👋</b>\n\nEscribe /help para ver todos mis comandos.\n\n<b>Bot creado por Bill Cypher Chk</b>");
 }
 
 //////////=========[Help Command]=========//////////
