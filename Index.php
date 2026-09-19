aee4e93971f134c55cd3d25dad413d476e229ba2
kaizostock014-dotcom <kaizostock014@gmail.com>
2026-09-19 09:09:59 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "5261916902:AAFhg0QN0s9weVpPzIZdnNihjQLRSwfXbrk"; // Enter ur bot token
+$botToken = "8848897705:AAEgO-XPMjdSjAmiOoigdxFmuVHidMIGPxw"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
