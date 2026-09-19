7e7baf4e053d82a03fb6dabcac0038778c8be602
kaizostock014-dotcom <kaizostock014@gmail.com>
2026-09-19 09:27:42 -0500

    Add bot token and admin ID to Index.php

--- Index.php
+++ Index.php
@@ -5,6 +5,7 @@
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
 $botToken = "8848897705:AAEgO-XPMjdSjAmiOoigdxFmuVHidMIGPxw"; // Enter ur bot token
+$adminId = "5320997298";
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
